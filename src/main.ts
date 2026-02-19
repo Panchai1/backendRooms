81d48d7ef2c77683ad155ada8ae1d7f739ac35b3
panachai1 <gromkromz@gmail.com>
2026-02-19 16:56:29 +0700

    first

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: 'http://localhost:3001', // port next.js
+    origin: 'http://localhost:3001', // port next.js sss
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
